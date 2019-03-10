d21166504375688fbf44d7bafbedbf203388dd82
githublu <yilu331@gmail.com>
2019-03-10 12:52:53 -0700

    remove unused files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-# TODO: remove setup.py before compile in pipeline server
-
-# step 1: create virtual env and activate it
-
-# step 2: install all worker related packages in the virtual env
-
-# step 3: install user requirements.txt 
-
-# step 4: download dataset
